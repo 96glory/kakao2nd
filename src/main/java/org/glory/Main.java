d889827187ca8d43eb674a976afe3340248a40f4
youngkwang.song <youngkwang.song@lgcns.com>
2022-10-07 16:59:46 +0900

    test2

--- src/main/java/org/glory/Main.java
+++ src/main/java/org/glory/Main.java
@@ -1,7 +1,9 @@
 package org.glory;
 
+import static org.glory.util.GsonUtils.getJsonStringFromObject;
 import static org.glory.util.GsonUtils.getListFromJsonString;
 
+import com.google.gson.JsonObject;
 import java.io.IOException;
 import java.util.List;
 import org.glory.model.TestVO;
@@ -11,6 +13,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("name", "glory");
+        jsonObject.addProperty("id", 1);
+        System.out.println(getJsonStringFromObject(jsonObject));
 
         try {
             String jsonString = HttpUtils.get("http://jsonplaceholder.typicode.com/posts");
--- src/main/java/org/glory/util/GsonUtils.java
+++ src/main/java/org/glory/util/GsonUtils.java
@@ -1,17 +1,18 @@
 package org.glory.util;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 // https://hianna.tistory.com/629
 // https://juntcom.tistory.com/126
-
+// https://hianna.tistory.com/629
 public class GsonUtils {
 
-    public static <T> String getJsonStringFromList(ArrayList<T> list) {
-        return new Gson().toJson(list);
+    public static String getJsonStringFromObject(Object object) {
+        return new Gson().toJson(object);
     }
 
     public static <T> List<T> getListFromJsonString(String jsonString, Class<T[]> clazz) {
